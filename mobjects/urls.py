827bd008a7ca598f2cf2637a60e630b26ca3eae1
Cyril Bécret <cyril.becret@gmail.com>
2020-01-27 10:19:08 +0100

    Add format suffixing to urls

--- mobjects/urls.py
+++ mobjects/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from mobjects import views
 
 urlpatterns = [
-    path('objects/', views.mobject_list),
-    path('objects/<int:pk>/', views.mobject_detail),
+    path('mobjects/', views.mobject_list),
+    path('mobjects/<int:pk>', views.mobject_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- mobjects/views.py
+++ mobjects/views.py
@@ -1,49 +1,47 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from mobjects.models import Mobject
 from mobjects.serializers import MobjectSerializer
 
-@csrf_exempt
-def mobject_list(request):
+@api_view(['GET', 'POST'])
+def mobject_list(request, format=None):
     """
     List all code mobjects, or create a new mobject.
     """
     if request.method == 'GET':
-        mobjects = Mobject.objects.all()
+        mobjects = mobject.objects.all()
         serializer = MobjectSerializer(mobjects, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MobjectSerializer(data=data)
+        serializer = MobjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def mobject_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def mobject_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code mobject.
     """
     try:
-        mobject = Mobject.objects.get(pk=pk)
+        mobject = mobject.objects.get(pk=pk)
     except Mobject.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = MobjectSerializer(mobject)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = MobjectSerializer(mobject, data=data)
+        serializer = MobjectSerializer(mobject, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         mobject.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
